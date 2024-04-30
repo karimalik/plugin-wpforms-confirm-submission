aec8654e6f5d21e0ab171c73d8e172923f8749f2
Karim Kompissi <40320510+karimalik@users.noreply.github.com>
2024-04-30 05:42:40 +0100

    Update wpforms-confirm-submission.php

--- wpforms-confirm-submission.php
+++ wpforms-confirm-submission.php
@@ -12,16 +12,24 @@ add_action('wpforms_process_complete', 'wpforms_confirm_submission', 10, 4);
 function wpforms_confirm_submission($fields, $entry, $form_data, $entry_id) {
     // Check if the form ID is 945 (replace 945 with the ID of your WPForms form)
     if ($form_data['id'] == 945) {
-        // Get the user's email from the form fields
-        $user_email = $fields['email'];
+        // Get the form object
+        $form = wpforms()->form->get($form_data['id']);
         
-        // Get the user's name from the form fields
-        $user_name = $fields['name'];
+        // Get the ID of the email field
+        $email_field_id = $form->get_field_id('email');
+        // Get the email from the fields using the ID
+        $user_email = $fields[$email_field_id];
+        
+        // Get the ID of the name field
+        $name_field_id = $form->get_field_id('name');
+        // Get the name from the fields using the ID
+        $user_name = $fields[$name_field_id];
         
         // Customize your confirmation message here
-        $message = "Dear $user_name,\n\nThank you for your confirmation. We have received your confirmation message..\n\nRegards,\nDiamand's Events";
+        $message = "Dear $user_name,\n\nThank you for your submission. We have received your message and will get back to you shortly.\n\nRegards,\nYour Website";
         
         // Send the confirmation email
         wp_mail($user_email, 'Confirmation of Form Submission', $message);
     }
 }
+
